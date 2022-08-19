65509b7915b740b84f655d446e0e79133a64c507
olegsmvl <olegsmvl2@mail.ru>
2022-08-20 01:42:21 +0300

    start neural network

---
+++ scripts/neural_network/simple_example.py
@@ -0,0 +1,11 @@
+# https://proglib.io/p/pishem-neyroset-na-python-s-nulya-2020-10-07
+
+import numpy as np
+
+def main():
+    var = np.zeros([3,3])
+    print(var)
+
+
+if __name__ == "__main__":
+    main()
