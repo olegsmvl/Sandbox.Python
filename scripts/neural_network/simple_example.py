2d5d99fe2ac645f42a0b5c6fab239a4befbe7786
olegsmvl <olegsmvl2@mail.ru>
2022-08-20 03:06:51 +0300

    install matplotlib and notebook, test it

--- scripts/neural_network/simple_example.py
+++ scripts/neural_network/simple_example.py
@@ -2,6 +2,18 @@
 
 import numpy as np
 
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+class Neuron:
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
+
+    def feedforward(self, inputs):
+        total = np.dot(self.weights, inputs) + self.bias
+        return sigmoid(total)
+
 def main():
     var = np.zeros([3,3])
     print(var)
