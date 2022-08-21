d2c3f2c5eed6b34cc24002ef2eda7d26d32f0419
olegsmvl <olegsmvl2@mail.ru>
2022-08-22 00:38:18 +0300

    add neural network

--- scripts/neural_network/network_from_zero/simple_example.py
+++ scripts/neural_network/network_from_zero/simple_example.py
@@ -11,6 +11,14 @@ class NeuralNetwork():
         self.h1 = Neuron(weights, bias)
         self.h2 = Neuron(weights, bias)
         self.o1 = Neuron(weights, bias)
+    def feedforward(self, x):
+        out_h1 = self.h1.feedforward(x)
+        out_h2 = self.h2.feedforward(x)
+        our_o1 = self.o1.feedforward(np.array(out_h1, out_h2))
+        return our_o1
+
+def mse_loss(y_true, y_pred):
+    return ((y_true - y_pred)** 2).mean()
 
 
 def test1():
@@ -23,10 +31,17 @@ def test1():
     print(n.feedforward(inputs))
 
 def test2():
-
+    network = NeuralNetwork()
+    x = np.array([2,3])
+    print(network.feedforward(x))
 
 def main():
     test2()
+
+    y_true = np.array([1, 0, 0, 1])
+    y_pred = np.array([0, 0, 0, 0])
+
+    print(mse_loss(y_true, y_pred))
 
 
 if __name__ == "__main__":
