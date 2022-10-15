1a91195546b483e1f0436beb5a4f57b01a4a4d61
olegsmvl <olegsmvl2@mail.ru>
2022-10-15 10:41:24 +0300

    matrix multiplication

--- scripts/neural_network/torch/tensor.py
+++ scripts/neural_network/torch/tensor.py
@@ -2,18 +2,26 @@ import torch
 
 def main():
     list = [[1,2,3],[4,5,6]]
-    xt = torch.tensor(list)
+    xt = torch.tensor(list).float()
     print(xt)
     print(xt.shape)
 
-    print("=========")
-    yt = torch.tensor(3.)
-    print(yt)
-    print(yt.shape)
+    # print("=========")
+    # yt = torch.tensor(3.)
+    # print(yt)
+    # print(yt.shape)
+    #
+    # print("=========")
+    # m = torch.ones(3,3,3)
+    # print(m)
 
     print("=========")
-    m = torch.ones(3,3,3)
+    m = torch.ones(3,2)
+    m = m*2
     print(m)
+    print(m.shape)
+    mul = torch.mm(xt, m)
+    print(mul)
 
 if __name__ == "__main__":
     main()
