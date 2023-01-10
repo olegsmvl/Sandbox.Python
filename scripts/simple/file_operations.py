1e362d5ce832235b50cea362e322715c806b14c0
olegsmvl <olegsmvl2@mail.ru>
2023-01-11 01:10:50 +0400

    text replace in file

--- scripts/simple/file_operations.py
+++ scripts/simple/file_operations.py
@@ -19,10 +19,21 @@ def update_file(file_name):
 def remove_file(file_name):
     os.remove(file_name)
 
+def replace_text(file_name, old_word, new_word):
+    f = open(file_name, 'r')
+    text = f.read()
+    f.close()
+
+    text = text.replace(old_word, new_word)
+
+    with open(file_name, 'w') as file:
+        file.write(text)
+
 def main():
     file_name = 'file.txt'
     create_file(file_name)
     update_file(file_name)
+    replace_text(file_name, 'line', 'block_buster')
     read_file(file_name)
     #remove_file(file_name)
 
