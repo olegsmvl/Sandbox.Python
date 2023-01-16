6290c4fc24f7023323c783b9a05eaf6f3defdd10
olegsmvl <olegsmvl2@mail.ru>
2023-01-17 00:36:14 +0400

    remove_duplicates

--- scripts/algorithm/course/remove_duplicates.py
+++ scripts/algorithm/course/remove_duplicates.py
@@ -2,11 +2,15 @@ def remove_duplicates(arr):
     i = 0
     next_non_duplicates = 1
     while(i < len(arr)):
+        if arr[next_non_duplicates - 1] != arr[i]:
+            arr[next_non_duplicates] = arr[i]
+            next_non_duplicates += 1
         i += 1
-    return  i
+    return  next_non_duplicates
 
 def main():
-    print(remove_duplicates([1,2,2,2]))
+    print(remove_duplicates([2, 3, 3, 3, 6, 9, 9]))
+    print(remove_duplicates([2, 2, 2, 11]))
 
 
 main()
