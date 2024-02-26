68e67d3ca41e08ee73b86691f1868d49ae897466
Oleg Smyslov <oleg.smyslov@evocargo.com>
2024-02-27 00:14:13 +0100

    upd eng dictionary

--- scripts/qt/qt6/simple.py
+++ scripts/qt/qt6/simple.py
@@ -1,18 +1,28 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox
 
 class SimpleWindow(QWidget):
     def __init__(self):
         super().__init__()
-
         self.init_ui()
 
     def init_ui(self):
         self.setWindowTitle('Simple Window')
         self.setGeometry(100, 100, 300, 200)
+        
+        self.button = QPushButton('Нажми меня', self)
+        self.button.setGeometry(100, 100, 100, 50)
 
-        self.show()
+        # Привязываем функцию к событию "clicked" (нажатие на кнопку)
+        self.button.clicked.connect(self.show_message)
+
+    def show_message(self):
+        # Создаем диалоговое окно с сообщением
+        QMessageBox.information(self, 'Сообщение', 'Кнопка была нажата!')
 
 if __name__ == '__main__':
     app = QApplication([])
     window = SimpleWindow()
+
+    window.show()
     app.exec()
