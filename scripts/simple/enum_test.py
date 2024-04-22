f96e97eb90a3b2cca5056979f4f1116bfab1b91d
ovegsmvl <ovegsmvl2@mail.ru>
2024-04-22 11:40:25 +0200

    enum tests

---
+++ scripts/simple/enum_test.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+
+class Nums(Enum):
+    FIRST = 1
+    SECOND = 2
+    THIRD = 3
+
+
+s = set()
+s.add(Nums.FIRST)
+s.add(Nums.SECOND)
+s.add(Nums.SECOND)
+
+for item in s:
+    print(item)
+
+for item in s:
+    print(item.value)
+
+s.add(Nums.THIRD)
+s_list = list(s)
+print(s_list[2])
+print(s_list[1])
+print(s_list[0])
